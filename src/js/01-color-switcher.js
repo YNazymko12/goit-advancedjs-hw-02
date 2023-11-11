0fb098a3dd8d5f36f40f2e6ae0cc85d7f206a4c2
YNazymko12 <yulychka12@gmail.com>
2023-11-11 15:07:56 +0100

    Feat(JS) Add code in task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const body = document.querySelector('body');
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
 
+let intervalId;
+
+startButton.addEventListener('click', handleStartClick);
+stopButton.addEventListener('click', handleStopClick);
+
+stopButton.disabled = true;
+
+function handleStartClick() {
+    startButton.disabled = true;
+    stopButton.disabled = false;
+
+    intervalId = setInterval(changeBackgroundColor, 1000);
+};
+
+function handleStopClick() {
+    startButton.disabled = false;
+    stopButton.disabled = true;
+
+    clearInterval(intervalId);
+};
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+    };
+
+function changeBackgroundColor() {
+    body.style.backgroundColor = getRandomHexColor();
+}
